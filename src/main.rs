925bb657035f55544b987fa6270b46ef48eddf83
mdjcosta30 <mdjcosta30@gmail.com>
2024-07-04 20:34:07 +0200

    End of episode 3
    Covered
    - Input
    - Player movement
    - More systems
    - More Queries
    - Spawning Enemies

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@
 // Assets are taken from kenney.nl
 use bevy::{prelude::*, window::PrimaryWindow};
 
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SIZE: f32 = 64.0; // Sprite size
+pub const NUMBER_OF_ENEMIES: usize = 4;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, (spawn_camera, spawn_player, spawn_enemies))
+        .add_systems(Update, (player_movement, confine_player_movement))
         .run();
 }
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemey {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -36,15 +43,96 @@ pub fn spawn_player(
     ));
 }
 
-pub fn spawn_camera(
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+pub fn spawn_enemies(
     mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
 
-    commands.spawn(
-        Camera2dBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-            ..default()
-    });
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let random_x = rand::random::<f32>() * window.width();
+        let random_y = rand::random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemey {},
+        ));
+    }
+}
+
+pub fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        // If there is movement, normalize (make smooth? Ensure stays constant?)
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        // Moves the player at same speed, no matter the framerate
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+// This is to keep the player in the screen,
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_tranform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_tranform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_tranform.translation = translation;
+    }
 }
