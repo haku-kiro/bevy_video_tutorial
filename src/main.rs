3f700349f5897c0de0f30a3513042d167bd5701c
mdjcosta30 <mdjcosta30@gmail.com>
2024-07-11 23:22:55 +0200

    End of episode 4
    Covered,
    - Enemy movement system
    - Sound effects (updated for 0.13.0
    - Pseudo-Collision detection
    - Despawning
    - More examples of ECS and Queries

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,24 @@ use bevy::{prelude::*, window::PrimaryWindow};
 pub const PLAYER_SPEED: f32 = 500.0;
 pub const PLAYER_SIZE: f32 = 64.0; // Sprite size
 pub const NUMBER_OF_ENEMIES: usize = 4;
+pub const ENEMY_SPEED: f32 = 200.0;
+pub const ENEMY_SIZE: f32 = 64.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_camera, spawn_player, spawn_enemies))
-        .add_systems(Update, (player_movement, confine_player_movement))
+        .add_systems(
+            Update,
+            (
+                player_movement,
+                confine_player_movement,
+                enemy_movement,
+                update_enemy_direction,
+                confine_enemy_movement,
+                enemy_hit_player,
+            ),
+        )
         .run();
 }
 
@@ -18,7 +30,9 @@ fn main() {
 pub struct Player {}
 
 #[derive(Component)]
-pub struct Enemey {}
+pub struct Enemy {
+    pub direction: Vec2,
+}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -69,7 +83,9 @@ pub fn spawn_enemies(
                 texture: asset_server.load("sprites/ball_red_large.png"),
                 ..default()
             },
-            Enemey {},
+            Enemy {
+                direction: Vec2::new(rand::random::<f32>(), rand::random::<f32>()).normalize(),
+            },
         ));
     }
 }
@@ -134,5 +150,123 @@ pub fn confine_player_movement(
         }
 
         player_tranform.translation = translation;
+    }
+}
+
+pub fn enemy_movement(mut enemy_query: Query<(&mut Transform, &Enemy)>, time: Res<Time>) {
+    for (mut transform, enemy) in enemy_query.iter_mut() {
+        let direction = Vec3::new(enemy.direction.x, enemy.direction.y, 0.0);
+        transform.translation += direction * ENEMY_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn update_enemy_direction(
+    mut commands: Commands,
+    mut enemy_query: Query<(&Transform, &mut Enemy)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>
+) {
+    let window = window_query.get_single().unwrap();
+
+    let half_enemy_size = ENEMY_SIZE / 2.0;
+    let x_min = 0.0 + half_enemy_size;
+    let x_max = window.width() - half_enemy_size;
+    let y_min = 0.0 + half_enemy_size;
+    let y_max = window.height() - half_enemy_size;
+
+    // If the enemy is moving off of the screen, flip the direction
+    for (transform, mut enemy) in enemy_query.iter_mut() {
+        let mut direction_changed = false;
+
+        let translation = transform.translation;
+        if translation.x <= x_min || translation.x >= x_max {
+            enemy.direction.x *= -1.0;
+            direction_changed = true;
+        }
+        if translation.y <= y_min || translation.y >= y_max {
+            enemy.direction.y *= -1.0;
+            direction_changed = true;
+        }
+
+        if direction_changed {
+            let sound_effect_1 = asset_server.load("audio/pluck_001.ogg");
+            let sound_effect_2 = asset_server.load("audio/pluck_002.ogg");
+
+            let sound_effect = if rand::random::<f32>() > 0.5 {
+                sound_effect_1
+            } else {
+                sound_effect_2
+            };
+
+            // This is how you play audio in later versions of bevy
+            commands.spawn(AudioBundle {
+                source: sound_effect,
+                ..default()
+            });
+
+            // Previously, you'd add the param audio: Res<Audio>, and do the following,
+            // audio.play(sound_effect);
+        }
+    }
+}
+
+pub fn confine_enemy_movement(
+    mut enemy_query: Query<&mut Transform, With<Enemy>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    let half_enemy_size = ENEMY_SIZE / 2.0;
+    let x_min = 0.0 + half_enemy_size;
+    let x_max = window.width() - half_enemy_size;
+    let y_min = 0.0 + half_enemy_size;
+    let y_max = window.height() - half_enemy_size;
+
+    for mut transform in enemy_query.iter_mut() {
+        let mut translation = transform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        transform.translation = translation;
+    }
+}
+
+// This is basic collision detection
+pub fn enemy_hit_player(
+    mut commands: Commands,
+    mut player_query: Query<(Entity, &Transform), With<Player>>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+    asset_server: Res<AssetServer>
+) {
+    if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
+        for enemy_transform in enemy_query.iter() {
+            let distance = player_transform.translation.distance(enemy_transform.translation);
+
+            let player_radius = PLAYER_SIZE / 2.0;
+            let enemy_radius = ENEMY_SIZE / 2.0;
+            if distance < player_radius + enemy_radius {
+                println!("Enemey hit player! Game Over!");
+
+                // Player explosion sound on being hit
+                let sound_effect = asset_server.load("audio/explosionCrunch_000.ogg");
+                commands.spawn(AudioBundle {
+                    source: sound_effect,
+                    ..default()
+                });
+
+                // We're despawning the player when they're hit
+                commands.entity(player_entity).despawn();
+            }
+        }
     }
 }
