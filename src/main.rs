80f0065b423c04e87e320c8cc9bc1f6a24a302ae
mdjcosta30 <mdjcosta30@gmail.com>
2024-07-19 22:56:16 +0200

    Full refactor into separate modules

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,27 @@
-pub mod components;
 pub mod events;
-pub mod resources;
 mod systems;
 
+use enemy::EnemyPlugin;
+use player::PlayerPlugin;
+use score::ScorePlugin;
+use star::StarPlugin;
+
 use events::*;
-use resources::*;
 use systems::*;
+
+pub mod enemy;
+mod player;
+pub mod score;
+pub mod star;
 
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .init_resource::<Score>()
-        .init_resource::<StarSpawnTimer>()
-        .init_resource::<EnemySpawnTimer>()
-        .init_resource::<HighScores>()
         .add_event::<GameOver>()
-        .add_systems(
-            Startup,
-            (spawn_camera, spawn_player, spawn_enemies, spawn_stars),
-        )
-        .add_systems(
-            Update,
-            (
-                player_movement,
-                confine_player_movement,
-                enemy_movement,
-                update_enemy_direction,
-                confine_enemy_movement,
-                enemy_hit_player,
-                player_hit_star,
-                update_score,
-                tick_star_spawn_timer,
-                spawn_stars_over_time,
-                tick_enemy_spawn_timer,
-                spawn_enemies_over_time,
-                exit_game,
-                handle_game_over,
-                update_high_scores,
-                high_scores_updated,
-            ),
-        )
+        .add_plugins((EnemyPlugin, PlayerPlugin, ScorePlugin, StarPlugin))
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, (exit_game, handle_game_over))
         .run();
 }
---
+++ src/star/mod.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+use resources::*;
+use systems::*;
+
+pub mod components;
+mod resources;
+mod systems;
+
+pub const NUMBER_OF_STARS: usize = 10;
+pub const STAR_SIZE: f32 = 30.0; // Sprite size 30x30
+
+pub struct StarPlugin;
+
+impl Plugin for StarPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<StarSpawnTimer>()
+            .add_systems(Startup, spawn_stars)
+            .add_systems(Update, (tick_star_spawn_timer, spawn_stars_over_time));
+    }
+}
