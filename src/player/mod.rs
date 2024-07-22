446115134e9adea5e8fb36fe4450d504d63a5bfa
mdjcosta30 <mdjcosta30@gmail.com>
2024-07-22 21:46:03 +0200

    End of episode 8
    Covered:
    - Explicit System ordering
    - System sets

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -4,20 +4,26 @@ use systems::*;
 pub mod components;
 mod systems;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player).add_systems(
-            Update,
-            (
-                player_movement,
-                confine_player_movement,
-                enemy_hit_player,
-                player_hit_star,
-            )
-                // Don't over use this; forcing systems to not run in parallel
-                .chain(),
-        );
+        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
+            .add_systems(Startup, spawn_player)
+            .add_systems(
+                Update,
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                    enemy_hit_player,
+                    player_hit_star,
+                ),
+            );
     }
 }
