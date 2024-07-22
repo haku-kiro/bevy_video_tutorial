600298fad45a37989a13b6899354e10017dc9aa4
mdjcosta30 <mdjcosta30@gmail.com>
2024-07-22 21:35:12 +0200

    Using chain - runs the systems in the order they appear inside a tuple

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -11,12 +11,12 @@ impl Plugin for PlayerPlugin {
         app.add_systems(Startup, spawn_player).add_systems(
             Update,
             (
-                // You can use before/after to run commands in order
-                player_movement.before(confine_player_movement),
+                player_movement,
                 confine_player_movement,
                 enemy_hit_player,
                 player_hit_star,
-            ),
+            )
+                .chain(),
         );
     }
 }
