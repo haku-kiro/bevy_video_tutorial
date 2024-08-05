1c086ea43811bf3ac1b852c3284e702eff61e081
mdjcosta30 <mdjcosta30@gmail.com>
2024-08-05 17:33:23 +0200

    Fix: setting state on game over

--- src/systems.rs
+++ src/systems.rs
@@ -51,8 +51,12 @@ pub fn exit_game(
     }
 }
 
-pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(
+    mut game_over_event_reader: EventReader<GameOver>,
+    mut next_state: ResMut<NextState<AppState>>,
+) {
     for event in game_over_event_reader.read() {
         println!("Your final score is: {}", event.score.to_string());
+        next_state.set(AppState::GameOver);
     }
 }
