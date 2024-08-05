43e56173455a6d533fb17addb82e17aa09d0de35
mdjcosta30 <mdjcosta30@gmail.com>
2024-08-05 15:08:15 +0200

    Toggling state
    Taken from the bevy cheatbook, the preferred way to toggle the state

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -3,17 +3,14 @@ use bevy::prelude::*;
 use crate::game::SimulationState;
 
 pub fn toggle_simulation(
-    mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
+    mut next_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.pressed(KeyCode::Space) {
-        // This was like simulat_state.0 == SimulationState::Running, that is however, a private
-        // member
-        let state = simulation_state.get();
-        if *state == SimulationState::Running {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
-            println!("Simulation Paused.");
+        match simulation_state.get() {
+            SimulationState::Running => next_state.set(SimulationState::Paused),
+            SimulationState::Paused => next_state.set(SimulationState::Running),
         }
     }
 }
