05a67826b79ee4c64be84e36a47a8a8fdf5d0e9a
mdjcosta30 <mdjcosta30@gmail.com>
2024-08-05 17:03:32 +0200

    Feat: handling star plugin with state

--- src/game/star/mod.rs
+++ src/game/star/mod.rs
@@ -2,6 +2,9 @@ use bevy::prelude::*;
 use resources::*;
 use systems::*;
 
+use crate::game::SimulationState;
+use crate::AppState;
+
 pub mod components;
 mod resources;
 mod systems;
@@ -14,7 +17,14 @@ pub struct StarPlugin;
 impl Plugin for StarPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<StarSpawnTimer>()
-            .add_systems(Startup, spawn_stars)
-            .add_systems(Update, (tick_star_spawn_timer, spawn_stars_over_time));
+            // Start spawning stars when the games starts, i.e. you enter Game state
+            .add_systems(OnEnter(AppState::Game), spawn_stars)
+            .add_systems(
+                Update,
+                (tick_star_spawn_timer, spawn_stars_over_time)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            )
+            .add_systems(OnExit(AppState::Game), despawn_stars);
     }
 }
